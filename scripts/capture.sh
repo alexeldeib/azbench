69c7cf9558e9c0eaf47c75b013ae2c10ce25d639
Alexander Eldeib <alexeldeib@gmail.com>
2020-09-18 22:11:43 -0700

    fix: displayNames and capture setup

--- scripts/capture.sh
+++ scripts/capture.sh
@@ -7,16 +7,16 @@ BASH_ROOT="$(dirname "${BASH_SOURCE[0]}")/.."
 cd "$BASH_ROOT"
 
 az keyvault secret download --vault-name aks-dataplane-test -n geneva-agent -e base64 -f cert.pfx
-openssl pkcs12 -nocerts -nodes -passin pass: -in cert.pfx -out gcskey.pem && openssl rsa -in gcskey.pem -out config/gcskey.pem
-openssl pkcs12 -nokeys -nodes -passin pass: -in cert.pfx -out gcscert.pem && openssl x509 -in gcscert.pem -out config/gcscert.pem
-rm gcskey.pem gcscert.pem cert.pfx
+openssl pkcs12 -nocerts -nodes -passin pass: -in cert.pfx -out gcskeytmp.pem && openssl rsa -in gcskeytmp.pem -out gcskey.pem
+openssl pkcs12 -nokeys -nodes -passin pass: -in cert.pfx -out gcscerttmp.pem && openssl x509 -in gcscerttmp.pem -out gcscert.pem
+rm gcskeytmp.pem gcscerttmp.pem cert.pfx
 
 echo "Starting metrics container"
 
 docker run \
     -d \
-    -v $(PWD)/config/gcscert.pem:/etc/certs/gcscert.pem \
-    -v $(PWD)/config/gcskey.pem:/etc/certs/gcskey.pem \
+    -v $(PWD)/gcscert.pem:/etc/certs/gcscert.pem \
+    -v $(PWD)/gcskey.pem:/etc/certs/gcskey.pem \
     --rm \
     --name geneva \
     alexeldeib/metrics:latest \
