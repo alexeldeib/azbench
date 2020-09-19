b24bab844203e437514ad8039e65041e5de4ab45
Alexander Eldeib <alexeldeib@gmail.com>
2020-09-19 01:31:06 -0700

    remove enable

--- scripts/cluster.sh
+++ scripts/cluster.sh
@@ -31,7 +31,6 @@ az aks create -g "${GROUP}" \
     -l "${LOCATION}" \
     -c 3 \
     -k 1.17.7 \
-    --enable-managed-identity \
     --enable-node-public-ip \
     --load-balancer-sku standard \
     --network-plugin azure \
