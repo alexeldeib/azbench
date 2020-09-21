0e9158b7b9e0f2d928572601d753975c80c40a55
Alexander Eldeib <alexeldeib@gmail.com>
2020-09-21 15:47:12 -0700

    add scheduler, reduce scale factor matrix

--- scripts/cluster.sh
+++ scripts/cluster.sh
@@ -7,6 +7,7 @@ NEWGRP="$(cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1)"
 export GROUP="${GROUP:=$NEWGRP}"
 export LOCATION="southcentralus"
 export CACHING="${CACHING:-}"
+export ACTION="${ACTION:-}"
 
 set -o errexit # has to be after cat /dev/urandom piece
 
@@ -91,3 +92,71 @@ else
 fi
 
 echo "Successfully provisioned workload cluster"
+
+function get_restarts() {
+    echo "$(kubectl get pod -o jsonpath="{.items[*].status.containerStatuses[0].restartCount}")"
+}
+
+function get_pod_phase() {
+    echo "$(kubectl get pod -o jsonpath="{.items[*].status.phase}")"
+}
+
+function get_node_status() {
+    echo "$(kubectl get node -o jsonpath="{.items[*].status.conditions[?(.reason=='KubeletReady')].status}")"
+}
+
+echo "Checking if we should apply tuning"
+
+if [[ -n "${ACTION}"]]; then
+    echo "Applying tuning manifests"
+    kustomize build manifests/nsenter | envsubst | kubectl apply -f - 
+
+    echo "Waiting for rollout"
+    kubectl rollout status daemonset/nsenter
+
+    echo "Waiting for all pods to have 1 reboot to ensure tuning applied"
+    POD_COUNT="$(kubectl get pod -o jsonpath="{.items[*].metadata.name}" | wc -w)"
+    ALL_RESTARTED=""
+    while [[ "${ALL_RESTARTED}" -ne "true" ]]; do
+        RESTARTS="$(get_restarts)"
+        if [[ "${RESTARTS}" == "${POD_COUNT}" ]]; then
+            echo "All pods restarted once"
+            ALL_RESTARTED="true"
+        else
+            echo "Found ${RESTARTS} pods, need ${POD_COUNT} to restart"        
+        fi
+    done
+
+    echo "Waiting for all pods to be ready after restart"
+    while [[ "${ALL_READY}" -ne "true" ]]; do
+        READY="$(get_pod_phase)"
+        READY_COUNT=0
+        for IS_READY in "${READY}"; do
+            if [[ "${IS_READY}" == "Running" ]]; then
+                READY_COUNT=$((READY_COUNT + 1))
+            fi
+        done
+        if [[ "${READY_COUNT}" == "${POD_COUNT}" ]]; then
+            echo "All pods ready after one restart"
+        then
+    done
+
+    NODE_COUNT="$(kubectl get node -o jsonpath="{.items[*].metadata.name}" | wc -w)"
+    echo "Waiting for all nodes to be ready after restart"
+    while [[ "${ALL_READY}" -ne "true" ]]; do
+        READY="$(get_node_status)"
+        READY_COUNT=0
+        for IS_READY in "${READY}"; do
+            if [[ "${IS_READY}" == "True" ]]; then
+                READY_COUNT=$((READY_COUNT + 1))
+            fi
+        done
+        if [[ "${READY_COUNT}" == "${NODE_COUNT}" ]]; then
+            echo "All nodes ready after one restart"
+        then
+    done
+
+    echo "Successfully completed all tuning and validation"
+else
+    echo "No tuning required."
+fi
