bae0744136799ab1231a93279246dbf0cf961cab
Alexander Eldeib <alexeldeib@gmail.com>
2020-09-21 23:31:57 -0700

    fix tuning checks

--- scripts/tuning.sh
+++ scripts/tuning.sh
@@ -30,31 +30,31 @@ if [[ -n "${ACTION}" ]]; then
     echo "Waiting for rollout"
     kubectl rollout status daemonset/nsenter
 
-    # echo "Waiting for all pods to have 1 reboot to ensure tuning applied"
-    # POD_COUNT="$(kubectl get pod -l app=nsenter -o jsonpath="{.items[*].metadata.name}" | wc -w)"
-    # ALL_RESTARTED="false"
-    # while [[ ! "${ALL_RESTARTED}" == "true" ]]; do
-    #     RESTARTS="$(get_restarts)"
-    #     COUNT=0
-    #     for IS_RESTARTED in "${RESTARTS}"; do
-    #         if [[ "${IS_RESTARTED}" == "1" ]]; then
-    #             COUNT=$((COUNT + 1))
-    #         fi
-    #     done
-    #     if [[ "${COUNT}" == "${POD_COUNT}" ]]; then
-    #         echo "All pods ready after one restart"
-    #         ALL_RESTARTED="true"
-    #     else
-    #         echo "Found ${COUNT} pods, need ${POD_COUNT} to restart"        
-    #     fi
-    # done
+    echo "Waiting for all pods to have 1 reboot to ensure tuning applied"
+    POD_COUNT="$(kubectl get pod -l app=nsenter -o jsonpath="{.items[*].metadata.name}" | wc -w)"
+    ALL_RESTARTED="false"
+    while [[ ! "${ALL_RESTARTED}" == "true" ]]; do
+        RESTARTS="$(get_restarts)"
+        COUNT=0
+        for IS_RESTARTED in ${RESTARTS}; do
+            if [[ "${IS_RESTARTED}" == "1" ]]; then
+                COUNT=$((COUNT + 1))
+            fi
+        done
+        if [[ "${COUNT}" == "${POD_COUNT}" ]]; then
+            echo "All pods ready after one restart"
+            ALL_RESTARTED="true"
+        else
+            echo "Found ${COUNT} pods, need ${POD_COUNT} to restart"        
+        fi
+    done
 
     echo "Waiting for all pods to be ready after restart"
     ALL_POD_READY="false"
-    while [[ ! "${ALL_POD_READY}" == "true" ]]; do
+    while [[ "${ALL_POD_READY}" != "true" ]]; do
         READY="$(get_pod_phase)"
         READY_COUNT=0
-        for IS_READY in "${READY}"; do
+        for IS_READY in ${READY}; do
             if [[ "${IS_READY}" == "Running" ]]; then
                 READY_COUNT=$((READY_COUNT + 1))
             fi
@@ -68,10 +68,10 @@ if [[ -n "${ACTION}" ]]; then
     echo "Waiting for all nodes to be ready after restart"
     NODE_COUNT="$(kubectl get node -o jsonpath="{.items[*].metadata.name}" | wc -w)"
     ALL_NODE_READY="false"
-    while [[ ! "${ALL_NODE_READY}" == "true" ]]; do
+    while [[ "${ALL_NODE_READY}" != "true" ]]; do
         READY="$(get_node_status)"
         READY_COUNT=0
-        for IS_READY in "${READY}"; do
+        for IS_READY in ${READY}; do
             if [[ "${IS_READY}" == "True" ]]; then
                 READY_COUNT=$((READY_COUNT + 1))
             fi
