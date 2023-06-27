6a3f2e099896f030b19cb67c2840e3e870eb6dc5
Antonio Villarreal <t-antoniovi@microsoft.com>
2023-06-27 13:06:01 -0700

    test new VMs

--- scripts/stressng.sh
+++ scripts/stressng.sh
@@ -55,18 +55,32 @@ tail -n 100 logs
 grep 'NotReady' logs
 ret=$?
 if [ "${ret}" == "1" ]; then
+  echo "Describing Node..."
+  printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
   kubectl describe node
+  printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+  echo
+
+  echo "Getting Events..."
   printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
   kubectl get events
   printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-  
+  echo
+
   echo "some nodes went not ready during run"
   exit ${ret}
 fi
-kubectl get node
+
+echo "Describing Node..."
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 kubectl describe node
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo
+
+echo "Getting Events..."
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 kubectl get events
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo
 
 echo "Successfully ran stressng without failures"
