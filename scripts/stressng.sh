b16a3fdc06413ea75a7b5d1a17242a13179c0df8
Antonio Villarreal <t-antoniovi@microsoft.com>
2023-07-18 11:49:20 -0700

    final checks - should fail or have problems #2

--- scripts/stressng.sh
+++ scripts/stressng.sh
@@ -62,10 +62,10 @@ echo "--------------------------------------------------------"
 echo "*** kubelet.service went down $kubelet_count times"
 echo "*** containerd.service went down $containerd_count times"
 echo "*** essential k8s services went unknown $unknown_count times" 
-echo "*** node went NotReady due to stress $((ret-1)) occurances"
+echo "*** node went NotReady due to stress $((ret-1)) times"
 echo "--------------------------------------------------------"
 
-if [ "${ret}" -gt 2 ]; then
+if [ "${ret}" -gt 1 ]; then
   echo "some nodes went not ready during run"
   exit ${1}
 fi
